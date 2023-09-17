e56f7891e20f57282ebb895083e2f7b17f544067
ChirujanoCodding <trdfabris@gmail.com>
2023-09-17 18:23:06 -0300

    feat: new command `/todo delete` added

--- src/commands/todos/create.rs
+++ src/commands/todos/create.rs
@@ -10,7 +10,7 @@ use poise::{serenity_prelude as serenity, CreateReply};
 
 use poise::ChoiceParameter;
 use serenity::ChannelType;
-use std::num::NonZeroU64;
+use std::{num::NonZeroU64, time::Duration};
 use tracing::error;
 
 /// Shortcut to create a to-do task
@@ -44,7 +44,11 @@ pub async fn create_ctx_menu(ctx: Context<'_>, msg: serenity::Message) -> Result
     Ok(())
 }
 
-#[poise::command(slash_command, subcommands("create", "update"), category = "Utilities")]
+#[poise::command(
+    slash_command,
+    subcommands("create", "update", "delete"),
+    category = "Utilities"
+)]
 pub async fn todo(_: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
@@ -93,7 +97,7 @@ pub enum Status {
     Finished = 1150602652904853564,
 }
 
-async fn is_forum_thread(ctx: Context<'_>) -> Result<bool, Error> {
+async fn is_forum_post(ctx: Context<'_>) -> Result<bool, Error> {
     let Ok(serenity::Channel::Guild(channel)) = ctx.channel_id().to_channel(ctx).await else {
         error!("❌ Cannot fetch the guild channel...");
         return Err("Error fetching guild channel".into());
@@ -121,7 +125,7 @@ async fn is_forum_thread(ctx: Context<'_>) -> Result<bool, Error> {
 }
 
 /// Change the status of any task
-#[poise::command(slash_command, check = "is_forum_thread", category = "Utilities")]
+#[poise::command(slash_command, check = "is_forum_post", category = "Utilities")]
 pub async fn update(
     ctx: Context<'_>,
     #[description = "the new tag to apply to the task"] status: Status,
@@ -155,5 +159,82 @@ pub async fn update(
                 .archived(status == Status::Finished),
         )
         .await?;
+    Ok(())
+}
+
+/// Delete a task
+#[poise::command(slash_command, check = "is_forum_post", category = "Utilities")]
+pub async fn delete(
+    ctx: Context<'_>,
+    #[description = "Skip the button confirmation."] force: Option<bool>,
+) -> Result<(), Error> {
+    let Ok(serenity::Channel::Guild(channel)) = ctx.channel_id().to_channel(ctx).await else {
+        error!("❌ Cannot fetch the guild channel...");
+        return Err("Error fetching guild channel.".into());
+    };
+
+    let force = force.unwrap_or_default();
+
+    let reply = CreateReply::new();
+    let embed = CreateEmbed::new();
+    let row = serenity::CreateActionRow::Buttons(vec![
+        serenity::CreateButton::new("confirm")
+            .style(serenity::ButtonStyle::Danger)
+            .label("Yes")
+            .emoji('✅'),
+        serenity::CreateButton::new("cancel")
+            .style(serenity::ButtonStyle::Secondary)
+            .label("No")
+            .emoji('❌'),
+    ]);
+
+    if !force {
+        let response = ctx
+            .send(
+                reply
+                    .clone()
+                    .embed(
+                        embed
+                            .clone()
+                            .color(Color::RED)
+                            .title("🤚 Wait. Are you sure to delete the task?")
+                            .description("> This operation is not reversible."),
+                    )
+                    .components(vec![row])
+                    .ephemeral(true),
+            )
+            .await?;
+        if let Some(interaction) = response
+            .message()
+            .await?
+            .await_component_interactions(ctx.discord().shard.clone())
+            .author_id(ctx.author().id)
+            .message_id(response.message().await?.id)
+            .timeout(Duration::from_secs(60))
+            .await
+        {
+            match interaction.data.custom_id.as_str() {
+                "cancel" => {
+                    response.delete(ctx).await?;
+                }
+                "confirm" => {
+                    channel.delete(ctx).await?;
+                }
+                _ => unreachable!(),
+            };
+        } else {
+            response.edit(ctx, reply
+                .embed(
+                    embed
+                        .color(Color::RED)
+                        .title("⌛ Oops... Timeout expired!")
+                        .description("> You did not interact with any button, so the action was cancelled."),
+                ).components(vec![])
+                .ephemeral(true)).await?;
+        }
+    } else {
+        channel.delete(ctx).await?;
+    }
+
     Ok(())
 }
