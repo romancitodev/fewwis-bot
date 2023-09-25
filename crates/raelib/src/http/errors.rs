89c28cdc20e7475a607ccfb2b28f6997c8f3dab2
ChirujanoCodding <trdfabris@gmail.com>
2023-09-25 11:30:07 -0300

    refactor(raelib): :building_construction: added `Display` and `Error` trait to `crate::error::Error`

--- crates/raelib/src/http/errors.rs
+++ crates/raelib/src/http/errors.rs
@@ -3,6 +3,18 @@ pub enum Error {
     Reqwest(reqwest::Error),
 }
 
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Reqwest(err) => {
+                write!(f, "{err:?}")
+            }
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
 impl From<reqwest::Error> for Error {
     fn from(value: reqwest::Error) -> Self {
         Self::Reqwest(value)
