f483acaf9013cac40b62365215637a180ac62eba
ChirujanoCodding <trdfabris@gmail.com>
2023-10-05 13:48:00 -0300

    feat: on construction

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![feature(exclusive_range_pattern)]
 
-use ::serenity::gateway::ActivityData;
+use ::serenity::{gateway::ActivityData, Client};
 use consts::OWNER_BOT;
 use helper::handle_error;
 use poise::serenity_prelude as serenity;
 use sea_orm::{ConnectionTrait, Database, Statement};
+use shuttle_poise::ShuttlePoise;
+use shuttle_runtime::Context as ShuttleContext;
+use shuttle_secrets::SecretStore;
 use std::collections::HashSet;
 use tracing::{error, info};
 mod api;
@@ -15,16 +18,21 @@ mod helper;
 mod types;
 use types::*;
 
-#[tokio::main]
-async fn main() {
-    if let Err(e) = dotenvy::dotenv() {
-        error!("❌ `dotenv`: {e}");
-        std::process::exit(0);
-    };
-
+#[shuttle_runtime::main]
+async fn main(#[shuttle_secrets::Secrets] store: SecretStore) -> ShuttlePoise<Data, Error> {
+    let bot_token = store
+        .get("BOT_TOKEN")
+        .context("❌ Missing BOT_TOKEN in Secrets.toml file")?;
+    let db_url = store
+        .get("DATABASE_URL")
+        .context("❌ Missing DATABASE_URL in Secrets.toml file")?;
+    let db_name = store
+        .get("DATABASE_URL")
+        .context("❌ Missing DATABASE_NAME in Secrets.toml file")?;
     tracing_subscriber::fmt::init();
-    poise::FrameworkBuilder::default()
-        .token(dotenvy::var("BOT_TOKEN").expect("❌ Missing BOT_TOKEN in .env file"))
+
+    let framework = poise::FrameworkBuilder::default()
+        .token(bot_token)
         .intents(serenity::GatewayIntents::all())
         .options(poise::FrameworkOptions {
             commands: commands::all(),
@@ -44,8 +52,53 @@ async fn main() {
             },
             on_error: |error| Box::pin(handle_error(error)),
             ..Default::default()
-        })
-        .setup(move |ctx, _ready, fm| {
+        });
+    // .setup(move |ctx, _ready, fm| {
+    //     Box::pin(async move {
+    //         info!("👷 Setting up the bot...");
+    //         ctx.set_activity(Some(ActivityData::playing("Casio Theme 4 life")));
+    //         info!("🕹 Setted activity.");
+    //         let commands = &fm.options().commands;
+    //         let create_commands = poise::builtins::create_application_commands(commands);
+
+    //         info!("🔁 Registering commands...");
+    //         serenity::Command::set_global_commands(ctx, create_commands).await?;
+    //         info!("📤 Registered commands.");
+    //         info!("📡 Connecting to database...");
+    //         let connection = Database::connect(db_url.clone()).await?;
+    //         connection
+    //             .execute(Statement::from_string(
+    //                 connection.get_database_backend(),
+    //                 format!("CREATE DATABASE IF NOT EXISTS `{}`;", db_name.clone()),
+    //             ))
+    //             .await?;
+    //         let connection = Database::connect(format!("{db_url}{db_name}")).await?;
+    //         info!("📡 Connection to database successfull.");
+    //         info!("✅ Bot initialized.");
+    //         Ok(Data::new(connection))
+    //     })
+    // });
+    let bot = poise::Framework::new(
+        poise::FrameworkOptions {
+            commands: commands::all(),
+            owners: HashSet::from([OWNER_BOT.into()]),
+            pre_command: |ctx| {
+                Box::pin(async move {
+                    info!(
+                        "📥 Starting interaction (command {})",
+                        &ctx.invoked_command_name()
+                    );
+                })
+            },
+            post_command: |ctx| {
+                Box::pin(
+                    async move { info!("✅ Command executed ({})", &ctx.invoked_command_name()) },
+                )
+            },
+            on_error: |error| Box::pin(handle_error(error)),
+            ..Default::default()
+        },
+        move |ctx, _ready, fm| {
             Box::pin(async move {
                 info!("👷 Setting up the bot...");
                 ctx.set_activity(Some(ActivityData::playing("Casio Theme 4 life")));
@@ -57,10 +110,6 @@ async fn main() {
                 serenity::Command::set_global_commands(ctx, create_commands).await?;
                 info!("📤 Registered commands.");
                 info!("📡 Connecting to database...");
-                let (db_url, db_name) = (
-                    dotenvy::var("DATABASE_URL")?,
-                    dotenvy::var("DATABASE_NAME")?,
-                );
                 let connection = Database::connect(db_url.clone()).await?;
                 connection
                     .execute(Statement::from_string(
@@ -73,8 +122,7 @@ async fn main() {
                 info!("✅ Bot initialized.");
                 Ok(Data::new(connection))
             })
-        })
-        .run()
-        .await
-        .unwrap();
+        },
+    );
+    Ok(bot.into())
 }
