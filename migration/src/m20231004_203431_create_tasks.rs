2b5dcece27b7a29ed3dfb7ebb33e3422875898e7
ChirujanoCodding <trdfabris@gmail.com>
2023-10-05 21:05:04 -0300

    feat(bot): :building_construction: finally created tables & relations

--- migration/src/m20231004_203431_create_tasks.rs
+++ migration/src/m20231004_203431_create_tasks.rs
@@ -6,42 +6,36 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(Task::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Post::Id)
+                        ColumnDef::new(Task::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(Task::PostId).big_unsigned().not_null())
+                    .col(ColumnDef::new(Task::Title).string().not_null())
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(Task::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+pub enum Task {
     Table,
     Id,
+    PostId,
     Title,
-    Text,
 }
--- migration/src/m20231004_203500_create_subtasks.rs
+++ migration/src/m20231004_203500_create_subtasks.rs
@@ -1,47 +1,55 @@
 use sea_orm_migration::prelude::*;
 
+use crate::m20231004_203431_create_tasks::Task;
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(Step::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Post::Id)
+                        ColumnDef::new(Step::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(Step::TaskId).integer().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("rel-post-subtask")
+                            .from(Step::Table, Step::Id)
+                            .to(Task::Table, Task::Id)
+                            .on_update(ForeignKeyAction::Cascade)
+                            .on_delete(ForeignKeyAction::Cascade),
+                    )
+                    .col(ColumnDef::new(Step::Index).big_integer().not_null())
+                    .col(ColumnDef::new(Step::Description).string().not_null())
+                    .col(ColumnDef::new(Step::Completed).boolean().not_null())
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(Step::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum Step {
     Table,
     Id,
-    Title,
-    Text,
+    TaskId,
+    Index,
+    Description,
+    Completed,
 }
